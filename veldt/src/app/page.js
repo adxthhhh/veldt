32c7205589b700ea3000c8960a5682207b2310f6
Adith A J <adithaj2006@gmail.com>
2026-06-22 15:49:10 +0000

    modified:   veldt/src/app/page.js

---
+++ veldt/src/app/page.js
@@ -0,0 +1,16 @@
+"use client";
+
+import React, { useState } from 'react';
+//importing framer motion later
+
+export default function Home() {
+ //adding userstate here later
+
+ return(
+ //the main div code will be here
+ <div className="h-screen w-screen overflow-hidden flex justify-center items-center">
+    
+ </div>
+
+ );
+}
